3211c45353ff75d37939337f761adb0b2a72e177
Wes Ladd <wsladd@icloud.com>
2021-10-29 10:29:35 -0500

    update

--- rand_accountid_generator.py
+++ rand_accountid_generator.py
@@ -3,7 +3,6 @@ import random as rand
 list_size = int(input("How large of a list should we create? "))
 rand_seed = rand.randint(1,10)
 with open (f'words-{rand_seed}.txt', 'a+') as file:
-    
     for i in range(0, list_size):
         rand_no = rand.randint(10**11, 10**12)
         file.write(str(rand_no)+'\n')
