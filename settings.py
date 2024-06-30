9b7bd509a983dc322dd0ddee99e72d0c0c987070
Wes Ladd <wesladd@traingrc.com>
2024-06-30 10:49:12 -0500

    removed lots of functionality and replace with web-ui and fastapi endpoints

--- settings.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import boto3
-
-def init(session):
-    sts = session.client('sts')
-    global scan_objects
-    scan_objects = []
-    global account_no
-    account_no = sts.get_caller_identity()['Account']
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="quiet_riot",
     version="1.0.6",
-    author="Wess ladd",
+    author="Wes ladd",
     author_email="wesladd@traingrc.com",
     description="Cloud Recon Tool",
     long_description=long_description,
